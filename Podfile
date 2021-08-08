654ebb27d6775f51bad7e6553629f9cd89d9749e
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2021-08-08 13:01:28 +0430

    Initial commit

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'loopPlayer (iOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for loopPlayer (iOS)
+  pod 'SwiftAudioPlayer'
+
+end
+
+target 'loopPlayer (macOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for loopPlayer (macOS)
+
+end
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,79 @@
 //
 
 import SwiftUI
+import SwiftAudioPlayer
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Hello, world!")
+                    .padding()
+                
+                Button(action: {
+                    guard let path = Bundle.main.path(forResource: "sample", ofType:"mp3") else {
+                        return }
+                    let url = URL(fileURLWithPath: path)
+                    
+                    SAPlayer.shared.setStartTime(at: 0.0)
+                    SAPlayer.shared.setEndTime(at: 5.0)
+                    SAPlayer.shared.play(url: url)
+                }, label: {
+                    Text("Play loop")
+                })
+                
+                Button(action: {
+                    SAPlayer.shared.pause()
+                }, label: {
+                    Text("Pause")
+                })
+            }
+            
+        }.onAppear(perform: {
+            _ = SAPlayer.Updates.PlayingStatus.subscribe({ url, state in
+                print("Player status \(state)")
+            })
+            
+            _ = SAPlayer.Updates.ElapsedTime.subscribe({ url, time in
+                print("Player ElapsedTime \(time)")
+                
+            })
+        })
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+var elpasedSub: UInt?
+var endTime: Double = -1
+var startTime: Double = 0
+
+extension SAPlayer {
+    
+    func setStartTime(at: Double) {
+        startTime = at
+        SAPlayer.shared.seekTo(seconds: at)
+    }
+    
+    func setEndTime(at: Double) {
+        endTime = at
+    }
+    
+    func play(url: URL) {
+        if elpasedSub != nil {
+            SAPlayer.Updates.ElapsedTime.unsubscribe(elpasedSub!)
+        }
+        elpasedSub = SAPlayer.Updates.ElapsedTime.subscribe({ url, time in
+            if time >= endTime {
+                SAPlayer.shared.seekTo(seconds: startTime)
+            }
+        })
+        
+        SAPlayer.shared.startSavedAudio(withSavedUrl: url)
+        SAPlayer.shared.play()
     }
 }
