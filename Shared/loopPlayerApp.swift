f2f406dfe256cfad708250defba7e4caf0f9d3a9
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2021-08-08 11:38:27 +0430

    Initial Commit

---
+++ Shared/loopPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  loopPlayerApp.swift
+//  Shared
+//
+//  Created by husseinhj on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct loopPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
